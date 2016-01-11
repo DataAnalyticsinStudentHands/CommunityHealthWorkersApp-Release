42b9c3474520e5ac355cfbe7a027089ecc648fb1
Carl Steven <carl_steven@live.com>
2016-01-11 14:15:52 -0600

    Updates: - docs - gulpfile - readme

---
+++ hooks/before_platform_add/add_plugins.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+//this hook installs all your plugins
+
+// add your plugins to this list--either
+// the identifier, the filesystem location
+// or the URL
+var pluginlist = [
+    "cordova-plugin-whitelist",
+    "cordova-plugin-inappbrowser"
+];
+
+// no need to configure below
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) {
+    console.log(stdout)
+}
+
+pluginlist.forEach(function(plug) {
+    exec("cordova plugin add " + plug, puts);
+});
